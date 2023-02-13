5df65d7e3710fac98d530cb16de97fd48a7cba27
tuberkulbek <a.burmaganov5@gmail.com>
2023-02-13 19:19:08 +0600

    header adaptive(needs fixes)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { refTypes } from "../../app/commonTypes"
 //@ts-ignore
 import logo from "../../assets/Logo.png"
@@ -5,40 +6,57 @@ import { Button } from "../Common/Button/Button"
 import './Header.css'
 
 const Header: React.FC<refTypes> = ({ home, about, stages, partners, contacts }) => {
+
+    const [hidden, setHidden] = useState('');
     const scrollToSection = (ref:React.RefObject<HTMLDivElement>) => {
         window.scrollTo({
             top: ref.current?.offsetTop,
             behavior: "smooth"
         })
     }
+
+    const handleShowNavBar = () => {
+        if (!hidden) {
+            setHidden('responsive')
+        } else {
+            setHidden('')
+        }
+    }
     
     return (
-        <div className="header">
-            <img src={logo} />
-            <div className="navigation_bar">
-                <button onClick={()=> scrollToSection(home)} className="button">
-                    Home
-                </button>
-                <button onClick={()=> scrollToSection(about)} className="button">
-                    About us
-                </button>
-                <button onClick={()=> scrollToSection(stages)} className="button">
-                    Stages
-                </button>
-                <button onClick={()=> scrollToSection(partners)} className="button">
-                    Partners
-                </button>
-                <button onClick={()=> scrollToSection(contacts)} className="button">
-                    Contacts
-                </button>
+        <div className={`header ${hidden}`}>
+            <div className="navigation_left">
+                <img src={logo} className="header_logo" />
+                <a className="icon" onClick={handleShowNavBar}>
+                    <i className="fa fa-bars"></i>
+                </a>
             </div>
-            <div className="login_bar">
-                <div>
-                    Log in
+            <div className={`navigation_right ${hidden}`}>
+                <div className="navigation_bar">
+                    <button onClick={()=> scrollToSection(home)} className="button">
+                        Home
+                    </button>
+                    <button onClick={()=> scrollToSection(about)} className="button">
+                        About us
+                    </button>
+                    <button onClick={()=> scrollToSection(stages)} className="button">
+                        Stages
+                    </button>
+                    <button onClick={()=> scrollToSection(partners)} className="button">
+                        Partners
+                    </button>
+                    <button onClick={()=> scrollToSection(contacts)} className="button">
+                        Contacts
+                    </button>
                 </div>
-                <Button>
-                    Register
-                </Button>
+                <div className="login_bar">
+                    <div>
+                        Log in
+                    </div>
+                    <Button>
+                        Register
+                    </Button>
+                </div>
             </div>
         </div>
     )
